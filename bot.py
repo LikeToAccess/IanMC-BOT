e61f05fecd6f355853f9b84c84f705ee429d0131
LikeToAccess <liketoaccess@protonmail.com>
2021-04-04 12:27:00 -0500

    Rename credentials file

--- bot.py
+++ bot.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 # filename          : bot.py
-# description       :
+# description       : 
 # author            : LikeToAccess
 # email             : liketoaccess@protonmail.com
 # date              : 04-04-2021
 # version           : v1.0
 # usage             : python bot.py
-# notes             :
+# notes             : 
 # license           : MIT
 # py version        : 3.8.2 (must run on 3.6 or higher)
 #==============================================================================
 import discord
 from functions import *
 
-allowed_users = read_file("credentials.md", filter=True)
-bot = commands.Bot(command_prefix=["please ", "!", "-", "ian!"], help_command=None, case_insensitive=True)
+allowed_users = read_file("credentials.txt", filter=True)
+bot = commands.Bot(command_prefix=['please ', '!', '-", "ian!"], help_command=None, case_insensitive=True)
 
 
 
